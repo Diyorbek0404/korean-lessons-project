d5354770292923721736f932e50e681631c2ba23
Diyorbek <77392009+Diyorbek0404@users.noreply.github.com>
2021-10-29 19:38:09 +0500

    Login yangilandi

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./Login.css"
+import {Link} from "react-router-dom"
+import rasmLogin from "../../components/rasm.png"
+
+const Login = () => {
+    return (
+        <div>
+            {/* <img src={rasm} alt="" /> */}
+            <section className="login">
+                <div className="login-form-div">
+                    <h1 className="login-title">Login</h1>
+                    <p className="login-subtitle">Saytdan to’liq foydalaning</p>
+                    <form className="login-form">
+                        <input type="email" className="login-input" placeholder="email" />
+                        <input type="password" className="login-input" placeholder="parol" />
+                        <Link className="form-subtitle" href="/">Parolni unutdingizmi?</<Link>
+                        <button className="form-login-btn">Kirish</button>
+                    </form>
+                    <Link href="/signup" className="link-reg">Ro’yxatdan o’tishni xoxlisizmi?</Link>
+                </div>
+
+                <div className="login-right">
+                    <h2 className="login-right-title">Biz birga ta’lim oling</h2>
+                    <p className="login-right-subtitle">Video kurslar faqat siz uchun</p>
+                    <img src={rasmLogin} className="login-right-img" alt="" />
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Login;
